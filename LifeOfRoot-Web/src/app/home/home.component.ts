e6327f563a4f3c6eafdfcb07d050e3158f9bade0
noenemy <noenemy@gmail.com>
2018-11-25 10:38:39 +0900

    Update home component UI

--- LifeOfRoot-Web/src/app/home/home.component.ts
+++ LifeOfRoot-Web/src/app/home/home.component.ts
@@ -18,9 +18,9 @@ export class HomeComponent implements OnInit {
   }
 
   animate(): void {
-    TweenMax.fromTo('h1', 3, {x:150}, {x:10});
-    TweenMax.fromTo('#p1', 2.4, {x:300}, {x:10});
-    TweenMax.fromTo('#p2', 3.3, {x:300}, {x:10});
+    TweenMax.fromTo('h1', 3, {x:250}, {x:0});
+    TweenMax.fromTo('#p1', 2.4, {x:300}, {x:0});
+    TweenMax.fromTo('#p2', 3.3, {x:300}, {x:0});
   }
 
   playGame() {
